09e346aab364a4fa04207a3ac29b99d25b949738
Anwesh43 <anweshthecool0@gmail.com>
2018-04-17 08:39:21 +0530

    adding SquareBallView in activity

--- app/src/main/java/ui/anwesome/com/kotlinsquareballview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsquareballview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinsquareballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.squareballview.SquareBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        SquareBallView.create(this)
     }
 }
