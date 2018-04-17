7b215e075daf604936a5bab1c976f4d998e963c8
Anwesh43 <anweshthecool0@gmail.com>
2018-04-17 08:01:48 +0530

    adding State class

--- squareballview/src/main/java/ui/anwesome/com/squareballview/SquareBallView.kt
+++ squareballview/src/main/java/ui/anwesome/com/squareballview/SquareBallView.kt
@@ -22,4 +22,26 @@ class SquareBallView(ctx : Context) : View(ctx) {
         }
         return true
     }
+    data class State (var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0){
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f, 0f, 0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                prevScale = scales[j] + dir
+                j += dir.toInt()
+                if (j == scales.size || j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    scales[j] = prevScale
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb :  () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
